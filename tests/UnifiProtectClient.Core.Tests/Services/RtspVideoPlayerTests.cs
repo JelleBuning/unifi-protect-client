da843bdb2cb9f67a4e53eeed869553f5999aabd3
Jelle Buning <Jellebuning@outlook.com>
2026-03-30 17:46:43 +0200

    Add GitHubActionsTestLogger; clean test usings

--- tests/UnifiProtectClient.Core.Tests/Services/RtspVideoPlayerTests.cs
+++ tests/UnifiProtectClient.Core.Tests/Services/RtspVideoPlayerTests.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Buffers;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
